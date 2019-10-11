bd48db9e7b069c22c1050e0cfe8eb8e38190e4fa
vjigouline <vjigouline@gmail.com>
2019-10-11 07:38:05 +0100

    Material and light types added.

---
+++ src/app/light-type.enum.ts
@@ -0,0 +1,8 @@
+export enum LightType {
+    AMBIENT = "Ambient",
+    DIRECTIONAL = "Directional",
+    HEMISPHERE = "Hemisphere",
+    POINT = "Point",
+    RECT_AREA = "Rectangular Area",
+    SPOT = "Spot"
+}
