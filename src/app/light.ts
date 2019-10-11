95cd1fc6f51c1158060ccc9a88e8a32447b6da4e
vjigouline <vjigouline@gmail.com>
2019-10-11 09:04:18 +0100

    Light class implementation

--- src/app/light.ts
+++ src/app/light.ts
@@ -1,8 +1,37 @@
 import { LightType } from './light-type.enum';
+import { uuid } from 'uuid'
 import * as THREE from 'three';
 
 export class Light {
     name: string;
     type: LightType;
     light: THREE.Light;
+
+    constructor(type: LightType) {
+        this.name = uuid();
+
+        switch(type) {
+            case LightType.AMBIENT:
+                this.light = new THREE.AmbientLight();
+                break;
+            case LightType.DIRECTIONAL:
+                this.light = new THREE.DirectionalLight();
+                break;
+            case LightType.HEMISPHERE:
+                this.light = new THREE.HemisphereLight();
+                break;
+            case LightType.POINT:
+                this.light = new THREE.PointLight();
+                break;
+            case LightType.RECT_AREA:
+                this.light = new THREE.RectAreaLight();
+                break;
+            case LightType.SPOT:
+                this.light = new THREE.SpotLight();
+                break;
+            default:
+                console.error("Invalid light type");
+                break;   
+        }
+    }
 }
