921a297de0aad8cebc6deb2cf7b4e2470bed9858
Vladimir Jigouline <vjigouline@gmail.com>
2020-04-14 15:45:07 +0100

    PointLightHelper class added

---
+++ src/app/objects3d/point-light-helper.spec.ts
@@ -0,0 +1,7 @@
+import { PointLightHelper } from './point-light-helper';
+
+describe('PointLightHelper', () => {
+  it('should create an instance', () => {
+    expect(new PointLightHelper()).toBeTruthy();
+  });
+});
