bafb43b30ce5b49019f47cf53ab881aa8901e9ec
Vladimir Jigouline <vjigouline@gmail.com>
2020-04-14 17:05:08 +0100

    HemisphereLightHelper class added

---
+++ src/app/objects3d/hemisphere-light-helper.spec.ts
@@ -0,0 +1,7 @@
+import { HemisphereLightHelper } from './hemisphere-light-helper';
+
+describe('HemisphereLightHelper', () => {
+  it('should create an instance', () => {
+    expect(new HemisphereLightHelper()).toBeTruthy();
+  });
+});
---
+++ src/app/objects3d/hemisphere-light-helper.ts
@@ -0,0 +1,32 @@
+import * as THREE from 'three';
+
+export class HemisphereLightHelper extends THREE.Object3D {
+
+    public positionSphere: THREE.Mesh;
+    private material: THREE.MeshPhysicalMaterial;
+
+    public light: THREE.HemisphereLight;
+    constructor(light: THREE.HemisphereLight) {
+        super();
+        this.light = light;
+        this.create3DObjects();
+    }
+
+    private create3DObjects() {
+        const geom = new THREE.SphereGeometry(20, 16, 16);
+        this.material = new THREE.MeshPhysicalMaterial({color: this.light.color,
+            emissive: this.light.color.clone().multiplyScalar(0.5)});
+        this.positionSphere = new THREE.Mesh(geom, this.material);
+        this.positionSphere.position.set(this.light.position.x,
+            this.light.position.y, this.light.position.z);
+        this.positionSphere.parent = this;
+        this.children.push(this.positionSphere);
+    }
+
+    public update(light: THREE.HemisphereLight): void {
+        this.positionSphere.position.set(light.position.x,
+            light.position.y, light.position.z);
+        this.material.color = light.color;
+        this.material.emissive.set(light.color.clone().multiplyScalar(0.5));
+    }
+}
