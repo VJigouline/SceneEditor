76453ec4b91633aae2aa466508ba6ad9cafd82c7
Vladimir Jigouline <vjigouline@gmail.com>
2020-04-16 18:06:36 +0100

    Confirmation dialog component added.

---
+++ src/app/user-controls/confirmation-dialog/confirmation-dialog.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, Inject, Input } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+export interface DialogData {
+  title: string;
+  label: string;
+  message: string;
+}
+
+@Component({
+  selector: 'app-confirmation-dialog',
+  templateUrl: './confirmation-dialog.component.html',
+  styleUrls: ['./confirmation-dialog.component.scss']
+})
+export class ConfirmationDialogComponent implements OnInit {
+
+  constructor(
+    public dialogRef: MatDialogRef<ConfirmationDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+
+  ngOnInit(): void {
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+}
--- src/app/user-controls/user-controls.module.ts
+++ src/app/user-controls/user-controls.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatInputModule } from '@angular/material/input';
+import { ConfirmationDialogComponent } from './confirmation-dialog/confirmation-dialog.component';
 
 
 @NgModule({
-  declarations: [Point3DComponent],
+  declarations: [Point3DComponent, ConfirmationDialogComponent],
   imports: [
     CommonModule,
     FormsModule,
@@ -17,7 +18,8 @@ import { MatInputModule } from '@angular/material/input';
     MatInputModule
   ],
   exports: [
-    Point3DComponent
+    Point3DComponent,
+    ConfirmationDialogComponent
   ]
 })
 export class UserControlsModule { }
