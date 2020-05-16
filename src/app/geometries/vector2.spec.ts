c5f0a69422ec82221477d4298c3afb5d5635d6ac
Vladimir Jigouline <vjigouline@gmail.com>
2020-05-16 16:17:15 +0100

    Class Vector2 created

---
+++ src/app/geometries/vector2.spec.ts
@@ -0,0 +1,7 @@
+import { Vector2 } from './vector2';
+
+describe('Vector2', () => {
+  it('should create an instance', () => {
+    expect(new Vector2()).toBeTruthy();
+  });
+});
---
+++ src/app/geometries/vector2.ts
@@ -0,0 +1,9 @@
+export class Vector2 {
+    public X = 0;
+    public Y = 0;
+
+    constructor(x: number, y: number) {
+        this.X = x;
+        this.Y = y;
+    }
+}
