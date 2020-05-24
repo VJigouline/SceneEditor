9a17f2e3fcb045ad5f619439f02a5cb375965a25
Vladimir Jigouline <vjigouline@gmail.com>
2020-05-24 17:25:19 +0100

    MeshPhysicalMaterial editor added.

--- src/app/materials/new-material/new-material.component.ts
+++ src/app/materials/new-material/new-material.component.ts
@@ -21,7 +21,7 @@ export class NewMaterialComponent implements OnInit {
     // { type: MaterialType.MESH_MATCAP, name: 'Mesh Matcap' },
     // { type: MaterialType.MESH_NORMAL, name: 'Mesh Normal' },
     { type: MaterialType.MESH_PHONG, name: 'Mesh Phong' },
-    // { type: MaterialType.MESH_PHYSICAL, name: 'Mesh Physical' },
+    { type: MaterialType.MESH_PHYSICAL, name: 'Mesh Physical' },
     { type: MaterialType.MESH_STANDARD, name: 'Mesh Standard' },
     // { type: MaterialType.MESH_TOON, name: 'Mesh Toon' },
     // { type: MaterialType.POINTS, name: 'Mesh Points' },
