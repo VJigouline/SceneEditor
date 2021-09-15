7e496019d99f582cee575a347f65fcd0480c51eb
vjigouline <vjigouline@gmail.com>
2021-09-15 21:04:50 +0100

    Graphics module added.

---
+++ src/app/graphics/iscene-node.ts
@@ -0,0 +1,42 @@
+import { Material, Matrix4, Object3D } from "three"
+
+export interface ISceneNode {
+    /** 
+     * Flag. If **true** - the object can be selected, **false** - cannot be selected.
+    */
+    Selectable : boolean;
+    /**
+     * Flag. If **true** - object selected, **false** - object not selected.
+     */
+    Selected : boolean;
+    /**
+     * Flag. If **true** - object visible, **false** - object not visible.
+     */
+    Visible : boolean;
+    /**
+     * Transformation to specify object location/orientation.
+     */
+    Transform : Matrix4;
+    /**
+     * Material for rendering.
+     */
+    Material : Material;
+    /**
+     * Reference to the parent object. If no parent object, then **null**
+     */
+    ParentNode : ISceneNode;
+    /**
+     * Flag. If **true**, then object will be updated during next redraw.
+     * This flag is set automatically, during object property update. It can be set manually
+     * for the low level control of the update.
+     */
+    IsDirty : boolean;
+    /**
+     * Automatically generated object ID.
+     */
+    readonly ID : string;
+    /**
+     * ThreeJS graphic object.
+     */
+    readonly GraphicObject : Object3D
+}
