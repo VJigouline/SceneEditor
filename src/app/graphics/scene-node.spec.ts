43ea0d1e78ebc3563600752682c594582874762d
vjigouline <vjigouline@gmail.com>
2021-09-16 18:55:40 +0100

    SceneNode class added

---
+++ src/app/graphics/scene-node.spec.ts
@@ -0,0 +1,7 @@
+import { SceneNode } from './scene-node';
+
+describe('SceneNode', () => {
+  it('should create an instance', () => {
+    expect(new SceneNode()).toBeTruthy();
+  });
+});
---
+++ src/app/graphics/scene-node.ts
@@ -0,0 +1,32 @@
+import { Matrix4, Material, Object3D } from "three";
+import { ISceneNode } from "./iscene-node";
+
+/** Basic implementation of the {@link ISceneNode} interface. */
+export class SceneNode implements ISceneNode {
+    public Selectable: boolean = true;
+    public Selected: boolean = false;
+    public get Visible(): boolean { return this._object.visible; };
+    public set Visible(value : boolean) { this._object.visible = value; }
+    public get Transform() : Matrix4 { return this._object.matrix; }
+    public set Transform(value : Matrix4) { this._object.matrix = value;}
+    public Material : Material;  // TODO: get/set material on objects depending on their type.
+    public Parent: ISceneNode;   // TODO: set as a child object on IGroupNode.
+    public get ID(): string { return this.GraphicObject.id.toString() }
+    public get GraphicObject(): Object3D { return this._object; }
+
+    private _object : Object3D;
+
+    /**
+     * Constructor.
+     * @param object {@link Object3D} object to display.
+     * @param material {@link Material} to assign. Optional.
+     * @param parent parent {@link ISceneNode} object. Optional.
+     * @param transfrom {@link Matrix4} object transformation matrix. Optional.
+     */
+    public constructor(object : Object3D, material? : Material, parent? : ISceneNode, transfrom? : Matrix4) {
+        this._object = object;
+        this.Material = material;
+        this.Parent = parent;
+        if (transfrom) this.GraphicObject.matrix = transfrom;
+    }
+}
