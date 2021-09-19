f6e5fe2d7e3752421d8f3580b7379359ff85bf53
vjigouline <vjigouline@gmail.com>
2021-09-19 16:30:16 +0100

    Point3 class comments and enhancements

--- src/app/geometries/point3.ts
+++ src/app/geometries/point3.ts
@@ -1,56 +1,107 @@
 import { Vector3 } from './vector3';
 
-export class Point3 {
+/**
+ * Class to specify 3D point.
+ */
+ export class Point3 {
     public X = 0;
     public Y = 0;
     public Z = 0;
 
-    constructor(x?: number, y?: number, z?: number) {
-        this.X = x ? x : 0;
-        this.Y = y ? y : 0;
-        this.Z = z ? z : 0;
+    /**
+     * Constructor
+     * @param x point X component
+     * @param y point Y component
+     * @param z point Z component
+    */
+    constructor(x = 0, y = 0, z = 0) {
+        this.set(x, y, z);
     }
+    /**
+     * Set Point3 components.
+     * @param x point X component
+     * @param y point Y component
+     * @param z point Z component
+     * @returns modified this point (for chaining)
+     */
+     public set(x = 0, y = 0, z = 0) : Point3 {
+        this.X = x;
+        this.Y = y;
+        this.Z = z;
 
+		return this;
+	}
+    /**
+     * Create {@link Vector3} from point1 to point2
+     * @param point1 first point
+     * @param point2 second point
+     * @returns created {@link Vector3} object.
+     */
     public static vector(point1: Point3, point2: Point3): Vector3 {
         return new Vector3(point2.X - point1.X, point2.Y - point1.Y, point2.Z - point1.Z);
     }
-
-    public add(vector: Vector3): Point3 {
+    /**
+     * Add {@link Vector3} to the current point
+     * @param vector {@link Vector3} to add.
+     * @returns modified this point (for chaining)
+     */
+    public addVector(vector: Vector3) : Point3 {
         this.X += vector.X;
         this.Y += vector.Y;
         this.Z += vector.Z;
         return this;
     }
-
-    public subtract(vector: Vector3): Point3 {
+    /**
+     * Subtract {@link Vector3} from the current point
+     * @param vector {@link Vector3} to subtract.
+     * @returns modified this point (for chaining)
+     */
+     public subtractVector(vector: Vector3): Point3 {
         this.X -= vector.X;
         this.Y -= vector.Y;
         this.Z -= vector.Z;
         return this;
     }
-
-    public multiply(value: number): Point3 {
+    /**
+     * Multiply all point components by number.
+     * @param value multiplier
+     * @returns modified this point (for chaining)
+     */
+     public multiplyScalar(value: number): Point3 {
         this.X *= value;
         this.Y *= value;
         this.Z *= value;
         return this;
     }
-
-    public divide(value: number): Point3 {
+    /**
+     * Divide all point components by number.
+     * @param value divider
+     * @returns modified this point (for chaining)
+     */
+     public divideScalar(value: number): Point3 {
         this.X /= value;
         this.Y /= value;
         this.Z /= value;
         return this;
     }
-
-    public clone(): Point3 {
+    /**
+     * Clone Point3 object to create new object with the same elements.
+     * @returns new Point3 object with the same elements as an original one.
+     */
+     public clone(): Point3 {
         return new Point3(this.X, this.Y, this.Z);
     }
+    /**
+     * Copy Point3 object into current one.
+     * @param point Point3 object to copy.
+     * @returns this Point3 object (for chaining.)
+     */
+     public copy(point: Point3): Point3 {
+        if (!point) { return; }
+        this.X = point.X;
+        this.Y = point.Y;
+        this.Z = point.Z;
 
-    public copy(vector: Point3): void {
-        if (!vector) { return; }
-        this.X = vector.X;
-        this.Y = vector.Y;
-        this.Z = vector.Z;
+        return this;
     }
 }
